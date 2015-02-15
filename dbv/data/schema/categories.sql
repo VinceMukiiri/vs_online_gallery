d25525b68bf22b6948c9d419be5b86941f822cda
VinceMukiiri <vincemukiiri@gmail.com>
2015-02-15 21:06:05 +0300

    Updated dbv

---
+++ dbv/data/schema/categories.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
